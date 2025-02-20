1fceafc84b01f85889ee989b52968a25859e28f3
oladapo <oladapoelijah49@gmail.com>
2025-02-20 20:11:25 +0100

    imole backend

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import ragRoutes from './routes/ragRoutes.js';
+import {connectDB} from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+connectDB()
 
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 
 app.use('/api', ragRoutes);
-
-app.get('/', (req, res) => res.send('Hello from Imole!'));
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
